c9d133a076c4ee91038e718071bfaef2a0454e22
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-09-24 14:12:13 +0530

    Addition of Header Component UI

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
         <link rel="icon" href="/airbnb.ico" />
       </Head>
 
-      {/*Header*/}
+      <Header />
 
       {/*Banner*/}
     </div>
