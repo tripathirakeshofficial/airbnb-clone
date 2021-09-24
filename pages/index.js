615ef69621a176c5c8d236a528b5530dc40e1020
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-09-24 15:43:44 +0530

    Addition of Banner Component UI

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
 
       <Header />
 
-      {/*Banner*/}
+      <Banner />
     </div>
   );
 }
