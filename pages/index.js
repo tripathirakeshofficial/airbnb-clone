f676044554a48943bd10b206202c78a8792baf4b
tripathirakeshofficial <rakeshtripathi408@gmail.com>
2021-09-24 17:10:49 +0530

    Addition of Footer Component UI

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Banner from "../components/Banner";
 import SmallCard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
 import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
 export default function Home({ exploreData, cardsData }) {
   return (
@@ -46,9 +47,11 @@ export default function Home({ exploreData, cardsData }) {
           img="https://links.papareact.com/4cj"
           title="The Greatest Outdoors"
           description="Wishlists curated by Airbnb"
-          buttonText="Get Inspired" 
+          buttonText="Get Inspired"
         />
       </main>
+
+      <Footer />
     </div>
   );
 }
